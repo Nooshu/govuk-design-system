7c67fba424da7e27ebbc026bcbd2eb8addb05a65
Matt Hobbs <matt@nooshu.com>
2019-05-02 10:38:04 +0100

    Add killswitch to the demo to wipe out SW if required

--- config/service-worker-src.js
+++ config/service-worker-src.js
@@ -1,43 +1,86 @@
-// Import from the CDN
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
+/* Allow ability to teardown the SW and caches if a buggy SW is deployed
+This will:
+- Install a blank SW (no-op)
+- Reload any tabs under SW control to "unbreak" them
+- Remove any existing SW caches for the given domain
+*/
+const KILLSWITCHENABLED = false;
 
-// explicitly load the workbox modules being used
-workbox.loadModule('workbox-cacheable-response');
+if (KILLSWITCHENABLED){
+  // install no-op service worker
+  self.addEventListener('install', () => {
+    // Skip over the "waiting" lifecycle state, to ensure that our
+    // new service worker is activated immediately, even if there's
+    // another tab open controlled by our older service worker code.
+    self.skipWaiting();
+  });
 
-// set some cache defaults
-workbox.core.setCacheNameDetails({
-  prefix: 'govuk-ds',
-  suffix: 'v1',
-  precache: 'install-time',
-  runtime: 'run-time'
-});
+  self.addEventListener('activate', activateEvent => {
+    // Optional: Get a list of all the current open windows/tabs under
+    // our service worker's control, and force them to reload.
+    // This can "unbreak" any open windows/tabs as soon as the new
+    // service worker activates, rather than users having to manually reload.
+    self.clients.matchAll({ type: 'window' }).then(windowClients => {
+      windowClients.forEach(windowClient => {
+        windowClient.navigate(windowClient.url);
+      });
+    });
 
-// modify SW update cycle
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
+    // delete all existing SW caches
+    activateEvent.waitUntil(
+      caches.keys()
+        .then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              return caches.delete(cacheName);
+            })
+          ).then(response => {
+            console.info('All service worker caches deleted');
+          })
+        })
+    );
+  });
+} else {
+  // Import from the CDN
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.2.0/workbox-sw.js');
 
-// place holder for any precaching
-workbox.precaching.precacheAndRoute([], {
-  cleanUrls: false,
-  urlManipulation: ({ url }) => {
-    // check for pathname that doesn't end in a '/' doesn't have a '.' (an extension)
-    if (url.pathname && !url.pathname.endsWith('/') && !url.pathname.includes('.')) {
-      console.warn('Rewriting!', url.pathname)
-      // if so rewrite the path to include slash
-      url['pathname'] = url['pathname'] + '/index.html'
-      return [url]
+  // explicitly load the workbox modules being used
+  workbox.loadModule('workbox-cacheable-response');
+
+  // set some cache defaults
+  workbox.core.setCacheNameDetails({
+    prefix: 'govuk-ds',
+    suffix: 'v1',
+    precache: 'precache',
+    runtime: 'run-time'
+  });
+
+  // modify SW update cycle
+  workbox.core.skipWaiting();
+  workbox.core.clientsClaim();
+
+  // place holder for any precaching
+  workbox.precaching.precacheAndRoute([], {
+    cleanUrls: false,
+    urlManipulation: ({ url }) => {
+      // check for pathname that doesn't end in a '/' doesn't have a '.' (an extension)
+      if (url.pathname && !url.pathname.endsWith('/') && !url.pathname.includes('.')) {
+        console.warn('Rewriting!', url.pathname)
+        // if so rewrite the path to include slash
+        url['pathname'] = url['pathname'] + '/index.html'
+        return [url]
+      }
+      return []
     }
-    return []
-  }
-});
+  });
 
-// Use workbox for the handling of static assets
-workbox.routing.registerRoute(
-  /\.(?:js|css|woff2)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'govuk-ds-static-resources',
-  })
-);
+  // Use workbox for the handling of static assets
+  workbox.routing.registerRoute(
+    /\.(?:js|css|woff2)$/,
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: 'govuk-ds-static-resources',
+    })
+  );
 
 // Use workbox for the handling of search results
 // workbox.routing.registerRoute(
@@ -68,3 +111,4 @@ workbox.routing.registerRoute(
 //     ]
 //   })
 // );
+}
